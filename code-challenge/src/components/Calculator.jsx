3dc5fdaa16e259906486dac7a20cf4a875497f4a
Camila Arruda <camilaam1802@gmail.com>
2021-10-21 19:11:33 -0300

    Criando lógica para calculo da área da parede

--- code-challenge/src/components/Calculator.jsx
+++ code-challenge/src/components/Calculator.jsx
@@ -2,9 +2,11 @@ import React, {useState} from "react";
 
 const Calculator = () => {
 
+    const portaArea = 1.52;
     const [wall1, setWall1] = useState([]);
     const [val1, setVall1] = useState(0);
     const [area1, setArea1] = useState(0);
+    const [porta, setPorta] = useState(false);
 
     // Salvar os dois valores da parede
     const handleClick = (val) => { 
@@ -14,7 +16,17 @@ const Calculator = () => {
         return setWall1(wall1);
     }
 
-    
+    // Calcular area da parede
+    const AreaCalculator = (wall) => {
+        const result = wall.reduce((acc,curr) => acc * curr)
+        if (porta === true) {
+           const resultTotal =  result - (portaArea);
+           console.log(resultTotal);
+           return setArea1(resultTotal);
+        }
+        console.log(result);
+        return setArea1(result);
+    };
 
     return (
         <div>
@@ -25,6 +37,12 @@ const Calculator = () => {
             <button type="button" onClick={() => handleClick(val1)}>
                 Click
             </button>
+            <button type="button" onClick={() => AreaCalculator(wall1)}>
+                Calcule
+                {console.log(porta)}
+            </button>
+            <label>Existe porta nessa parede?</label>
+            <input type="checkbox" name="porta" id="" value={porta} onClick= {() => setPorta(!porta)} />
         </div>
     );
 };
