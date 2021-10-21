c288e6e076f5689310cc26abc22eebc0f1f46989
Camila Arruda <camilaam1802@gmail.com>
2021-10-21 18:29:24 -0300

    Implementando logica gravar valores da parede

--- code-challenge/src/components/Calculator.jsx
+++ code-challenge/src/components/Calculator.jsx
@@ -1,8 +1,31 @@
-import React from "react";
+import React, {useState} from "react";
 
 const Calculator = () => {
+
+    const [wall1, setWall1] = useState([]);
+    const [val1, setVall1] = useState(0);
+    const [area1, setArea1] = useState(0);
+
+    // Salvar os dois valores da parede
+    const handleClick = (val) => { 
+        // convertendo a string em number
+        wall1.push(parseFloat(val));
+        console.log(wall1);
+        return setWall1(wall1);
+    }
+
+    
+
     return (
-        <div>OI Camila</div>
+        <div>
+            <label htmlFor=""> largura da primeira parede</label>
+            <input type="number" onChange={event => setVall1(event.target.value)} />
+            <label htmlFor=""> altura da primeira parede</label>
+            <input type="number" onChange={event => setVall1(event.target.value)} />
+            <button type="button" onClick={() => handleClick(val1)}>
+                Click
+            </button>
+        </div>
     );
 };
 
